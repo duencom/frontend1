ceccad1c3d4648f7eb34e9ef5a21ca003c005bf2
durncom <duenloi123@gmail.com>
2021-07-21 09:25:36 +0700

    comv3

---
+++ pages/[user].js
@@ -0,0 +1,8 @@
+import React from 'react';
+const UserPage = () => (
+ <h1 style={{ color: "blue", textAlign: "center" }}>
+ dynamic route page
+ import { useRouter } from 'next/router'
+ </h1>
+)
+export default UserPage
